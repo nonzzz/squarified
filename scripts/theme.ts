516a35a8cfb81b28fd2a7ff69378366c2fe66756
kanno <812137533@qq.com>
2025-02-03 11:08:34 +0800

    chore: stat

--- scripts/theme.ts
+++ scripts/theme.ts
@@ -4,15 +4,26 @@ export type Theme = 'light' | 'dark'
   const doc = document.documentElement
   const docDataset = doc.dataset
 
+  const themeButton = document.querySelector<HTMLButtonElement>('#theme-toggle')
+
+  if (themeButton) {
+    themeButton.addEventListener('click', toggleTheme)
+  }
+
   function updateTeme(theme: Theme) {
     localStorage.setItem('theme', theme)
     docDataset.theme = theme
     console.log(`Theme set to ${theme}`, docDataset)
   }
 
+  function toggleTheme() {
+    const theme = docDataset.theme === 'light' ? 'dark' : 'light'
+    updateTeme(theme)
+  }
+
   if (darkMediaQuery.matches) {
     if (docDataset.theme !== 'light') {
-      //   updateTeme('dark')
+      updateTeme('dark')
     }
   }
 })()

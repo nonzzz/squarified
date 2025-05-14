88322f755c2f24a3a1875b9183d237437ab0bdf3
kanno <812137533@qq.com>
2025-05-14 23:58:04 +0800

    chore: export plugins

--- src/index.ts
+++ src/index.ts
@@ -135,3 +135,5 @@ export type { Plugin, PluginContext, PluginHooks } from './shared/plugin-driver'
 export { definePlugin } from './shared/plugin-driver'
 
 export type { ExposedEventCallback, ExposedEventDefinition, PrimitiveEventMetadata } from './dom-event'
+
+export * from './plugins'
